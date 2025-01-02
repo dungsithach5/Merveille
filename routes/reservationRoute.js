d257bfb677cbd047000da6c367c78a0919cdb072
dungsithach5 <159418327+dungsithach5@users.noreply.github.com>
2025-01-02 14:30:03 +0700

    update route

--- routes/reservationRoute.js
+++ routes/reservationRoute.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import Reservation from '../models/reservation.model.js'
 const router = express.Router();
 
 import reservationController from '../controllers/reservation.controller.js';
@@ -28,6 +29,16 @@ router.post('/reservation', async (req, res) => {
         res.status(201).json({ message: 'Reservation created successfully!' });
     } catch (error) {
         res.status(500).json({ message: 'Error saving reservation', error });
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const reservation = await Reservation.findById(id);
+        res.json(reservation);
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error' });
     }
 });
 
